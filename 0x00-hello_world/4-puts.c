8ca84b1a3f7efac115a4abea4e55032f1e45c4be
fa-154 <fa6841968@gmail.com>
2023-03-16 09:17:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stido.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
