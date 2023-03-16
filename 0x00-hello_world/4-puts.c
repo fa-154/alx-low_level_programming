926b0b77b1179d6a635613a4a5f87314e2215626
fa-154 <fa6841968@gmail.com>
2023-03-16 10:09:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stido.h>
+
 /**
- * main - A program that prints a line with puts function
+ * main - Entry point 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
